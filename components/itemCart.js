e857eaa3d0b8ed0491058c31f0fa9630cb262ad7
Denis <denis_shakhov@vk.com>
2022-12-21 17:30:53 +0300

    fixed the items issue

--- components/itemCart.js
+++ components/itemCart.js
@@ -37,7 +37,7 @@ export default function ItemCart({id, title,price,img,description, weight}){
             animate={{ opacity: 1}}
             transition={{duration: .2}}
             exit={{ opacity: 0 }}
-             className="flex relative  my-2   2xl:p-0   h-full w-full  rounded  flex-row md:flex-col items-center justify-center">
+             className="flex relative  my-2   2xl:p-0   h-full w-full  rounded  flex-row md:flex-col items-center justify-start">
             <div className="flex items-center min-h-[240px] w-[350px] mt-1 justify-center w-[50%] md:w-full">
                 <Image className="" src={img} alt={title} height={180} width={380}/>
 
@@ -45,10 +45,10 @@ export default function ItemCart({id, title,price,img,description, weight}){
         <div className="absolute top-[-10px] right-2 ">
             <span className="font-[400] text-[18px] text-[#CBCBCB]">{weight} Г</span>
         </div>
-        <div className="flex  items-center justify-center flex-col pr-2 w-[50%] md:w-full">
-            <div className="flex  items-center     font-normals justify-center  text-center md:text-center font-normal sm:text-[32px] text-[24px]"><h4>{title}</h4></div>
-            <p className="flex text-center items-center  py-3 justify-center text-[18px] text-[#868484] min-h-[50px]"><h5>{description}</h5></p>
-            <div className="flex items-center justify-between  w-full md:px-2">
+        <div className="flex flex-col items-center justify-between h-full   pr-2 w-[50%] md:w-full">
+            <div className="flex  items-center     font-normals justify-start text-center md:text-center font-normal pb-auto sm:text-[32px] text-[24px]"><h4>{title}</h4></div>
+            <p className="flex text-center items-center  mb-auto py-0 justify-center text-[18px] text-[#868484] min-h-[50px]"><h5>{description}</h5></p>
+            <div className="flex items-center justify-between w-full md:px-2">
                 <span className="md:p-3   sm:ml-0 sm:px-4 flex items-center justify-center whitespace-nowrap rounded text-[#313131] sm:text-[28px] text-[24px]">{price} ₽</span>
                 {count === undefined && <div className="outline outline-2 md:py-2 outline-[#FF8932] md:my-1 whitespace-nowrap  sm:px-4 p-2 text-black  duration-75 active:scale-105 select-none  sm:py-1 flex justify-center sm:p-5 hover:text-white items-center hover:bg-[#FF8932] rounded-[90px]" onClick={()=> onClickAdd(id,title,price,img) }>Выбрать {count}</div>}
                 {count !== undefined && count > 0 &&
